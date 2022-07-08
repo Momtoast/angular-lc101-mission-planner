15e4b3012c181965bc3e43c1da9a050950e8c613
Melissa Matos <melissamomtoast@gmail.com>
2022-07-07 21:43:55 -0400

    couldn't get the image to show, even using soluion

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -25,5 +25,13 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
-
+  addCrewMember(candidate) {
+    if (this.crew.includes(candidate) === false) {
+      if (this.crew.length<3) {
+        this.crew.push(candidate);
+        }
+      } else {
+        this.crew.splice(this.crew.indexOf(candidate), 1);
+    }
+  }
 }
